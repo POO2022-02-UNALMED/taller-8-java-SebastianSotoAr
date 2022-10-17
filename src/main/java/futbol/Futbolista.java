d0e8be9fd228b5407396995685bd0da15c0cdcd7
SebastianSotoAr <ssotoa@unal.edu.co>
2022-10-17 13:01:00 -0500

    try 1

---
+++ src/main/java/futbol/Futbolista.java
@@ -0,0 +1,59 @@
+package futbol;
+
+public abstract class Futbolista implements Comparable<Futbolista> {
+	private String nombre;
+	private int edad;
+	private final String posicion;
+	
+	public Futbolista(String nombre, int edad, String posicion) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.posicion = posicion;
+	}
+
+	public Futbolista() {
+		this.nombre = "Maradona";
+		this.edad = 30;
+		this.posicion = "delantero";
+	}
+
+	@Override
+	public String toString() {
+		return "El futbolista " + nombre + " tiene " + edad + ",y juega de " + posicion;
+	}
+
+	@Override
+	public int compareTo(Futbolista o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
+	
+	public abstract boolean jugarConLasManos();
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getPosicion() {
+		return posicion;
+	}
+	
+}
